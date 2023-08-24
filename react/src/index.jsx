4f8db75dd21f32618a3df63c12a36c72a90af09f
NathanPerezR <88894841+NathanPerezR@users.noreply.github.com>
2023-08-24 01:30:20 -0500

    Fixed style import

--- react/src/index.jsx
+++ react/src/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import Header from "./Header/Header.jsx"
+import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header></Header>
   </React.StrictMode>,
   document.getElementById("root")
 );
