0f309962911b02294b5166a2088dba93203be49c
John Langs <johnslangsiii@gmail.com>
2023-08-27 06:29:49 +0000

    Integrated backend call in React Header

--- react/src/Header/Header.jsx
+++ react/src/Header/Header.jsx
@@ -1,42 +1,55 @@
+import React, { useState, useEffect, useCallback } from "react";
 import "./HeaderStyle.css"
 import "../Context/Context";
 
 // contains the sum of all accounts, banks info is pulled from with info button, and selector
 function Header(props) {
+  const [balance, setBalance] = useState(null);
+  const [accounts, setAccounts] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   // gets the total of all credits and debts
-  function getAccountSumString() {
-    
-    //TODO: tmp solution
-    let totalBalance = 88888888
+  const getAccountSum = React.useCallback(async () => {
+    setLoading(true);
+    const response = await fetch("/api/totalBalance", {});
+    const data = await response.json();
+    console.log(data);
+    let balance = data.amount;
+    balance = "$ " + balance.toLocaleString();
+    setBalance(balance);
+    setLoading(false);
+  }, [setBalance, setLoading]);
 
-    // formats commas to match local string
-    totalBalance = totalBalance.toLocaleString()
-    
-    // auto formats to "$ -value" if negitive
-    return "$ " + totalBalance;
-  }
 
   // gets the string of banks, displayed under total sum
-  function getBankNameString() {
-    
-    // TODO: tmp solution
-    let bankList = "JPMorgan Chase Bank, Bank of America, Wells Fargo Bank, Citibank, Goldman Sachs, Morgan Stanley, U.S. Bank, PNC Bank, TD Bank, Capital One, HSBC Bank, Santander Bank, BB&T (now part of Truist), SunTrust (now part of Truist), Regions Bank, Fifth Third Bank, Ally Bank, KeyBank, BMO Harris Bank, Citizens Bank, Comerica Bank, Union Bank, M&T Bank, Huntington Bank, Discover Bank, Charles Schwab Bank, TIAA Bank, First Republic Bank, Santander Bank, Synovus Bank, Frost Bank"
+  const getBankNameString = React.useCallback(async () => {
+    setLoading(true);
+    const response = await fetch("/api/accountsList");
+    const data = await response.json();
+    console.log(data);
+    const accounts = data.accounts_names_list.join(", ");
+    setAccounts(accounts);
+    setLoading(false);
+  }, [setAccounts, setLoading]);
 
-
-    let finalString = bankList;
-    return finalString;
-  }
+  useEffect(() => {
+    if (balance == null) {
+      getAccountSum();
+    }
+    if (accounts == null) {
+      getBankNameString();
+    }
+  }, [accounts, balance, getAccountSum, getBankNameString, setLoading])
 
   // returns rendered Header
   return (
     <section class = "HeaderContainer">
-        <p id="accountSum"> {getAccountSumString()} </p>
-        
-        <p id="bankList"> 
-          <b> Accounts Connected: </b>
-          {getBankNameString()} 
-        </p>
+      {!loading && balance != null &&
+        (<p id="accountSum"> {balance} </p>)
+      }
+      {!loading && accounts != null &&
+        (<p id="bankList"> <b> Accounts Connected: </b> {accounts} </p>)
+      }
     </section>
   )
 }
